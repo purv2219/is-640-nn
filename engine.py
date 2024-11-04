c098a33466981fe1c6dcbf7b12f5375e59f88568
purv2219 <88077869+purv2219@users.noreply.github.com>
2024-11-03 20:11:33 -0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -6,7 +6,7 @@ class Value:
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
-        self._backward = lambda: None
+        self._backward = lambda: None 
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
